f0b6be307793847de42e8cfd5d44d12a685213b2
FT-ONI <ismaeljc2005@gmail.com>
2026-04-24 02:07:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("¡Servidor de TechMarket iniciado correctamente en el contenedor!");
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('¡Servidor de TechMarket funcionando en EKS!\n');
+});
+
+server.listen(port, () => {
+  console.log(`¡Servidor de TechMarket iniciado y escuchando en el puerto ${port}!`);
+});
